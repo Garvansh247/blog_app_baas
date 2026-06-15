4e654bef01f588cb7192892a07603b7ed9dde75b
Garvansh <garvansh247@gmail.com>
2026-06-15 17:04:27 +0530

    fix(state): sanitize Appwrite objects before storing in Redux

--- src/features/postSlice.js
+++ src/features/postSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState={
     allPosts: [], // to store all the posts
+    activePosts: [], // to store only active posts
     myPosts: [] // to store posts of logged in user
 
 }
@@ -12,16 +13,22 @@ const postSlice=createSlice(
         initialState,
         reducers:{
                 setAllPosts(state,action){
-                    state.allPosts=action.payload;
+                    state.allPosts = JSON.parse(JSON.stringify(action.payload));
                 },
                 setMyPosts(state,action){
-                    state.myPosts=action.payload;
+                    state.myPosts=JSON.parse(JSON.stringify(action.payload));
+                },
+                setActivePosts(state,action){
+                    state.activePosts=JSON.parse(JSON.stringify(action.payload));
                 },
                 addToAllPosts(state,action){
                     state.allPosts.push(action.payload);
                 },
                 addToMyPosts(state,action){
                     state.myPosts.push(action.payload);
+                },
+                addToActivePosts(state,action){
+                    state.activePosts.push(action.payload);
                 },
                 updatePostInAllPosts(state,action){
                     const updatedPost=action.payload;
@@ -31,6 +38,10 @@ const postSlice=createSlice(
                     const updatedPost=action.payload;
                     state.myPosts=state.myPosts.map((post)=>post.$id===updatedPost.$id? updatedPost : post);
                 },
+                updatePostInActivePosts(state,action){
+                    const updatedPost=action.payload;
+                    state.activePosts=state.activePosts.map((post)=>post.$id===updatedPost.$id? updatedPost : post);
+                },
                 deletePostFromAllPosts(state,action){
                     const deletedPostId=action.payload;
                     state.allPosts=state.allPosts.filter((post)=>post.$id!==deletedPostId);
@@ -38,13 +49,15 @@ const postSlice=createSlice(
                 deletePostFromMyPosts(state,action){
                     const deletedPostId=action.payload;
                     state.myPosts=state.myPosts.filter((post)=>post.$id!==deletedPostId);
+                },
+                deletePostFromActivePosts(state,action){
+                    const deletedPostId=action.payload;
+                    state.activePosts=state.activePosts.filter((post)=>post.$id!==deletedPostId);
                 }
-
-                
         }
     }
 )
 
-export const {setAllPosts,setMyPosts,addToAllPosts,addToMyPosts,updatePostInAllPosts,updatePostInMyPosts,deletePostFromAllPosts,deletePostFromMyPosts}=postSlice.actions;
+export const {setAllPosts,setMyPosts,setActivePosts,addToAllPosts,addToMyPosts,addToActivePosts,updatePostInAllPosts,updatePostInMyPosts,updatePostInActivePosts,deletePostFromAllPosts,deletePostFromMyPosts,deletePostFromActivePosts}=postSlice.actions;
 
 export default postSlice.reducer;
